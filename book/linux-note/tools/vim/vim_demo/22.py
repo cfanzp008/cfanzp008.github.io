4653a46100b817c02c58333d6b66720ec13bcc52
alex <cfanzp008@gmail.com>
2026-05-28 17:39:25 +0800

    publish

---
+++ book/linux-note/tools/vim/vim_demo/22.py
@@ -0,0 +1,6 @@
+def fib(n):
+    a, b = 0, 1
+    while a < N:
+        print(a)
+        a, b = b, a+b
+fib(42)
---
+++ book/linux-note/tools/vim/vim_demo/26.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("hello A!\n");;;;;;
+    printf("hello BCD!\n");;;;;;
+    printf("hello World!\n");;;;;;
+    printf("hello s Worldaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbvvvvvvvvvvvvvvvvvvvv:w!\n");;;;;;
+    return 0;
+}
